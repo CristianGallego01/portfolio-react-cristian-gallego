5d84f79e4a1c687cf1384e38b9f8b15761cb4fc6
zowwii <cristiancmgallego@gmail.com>
2024-10-28 12:41:50 -0500

    hedaer and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Home } from "./components/Pages/Home/Home"
+import { Header } from "./components/Layouts/Header/Header"
 export const App = ()=> {
 
 
   return (
     <>
-    <Home/>
+    <Header/>
+
     </>
   )
 }
