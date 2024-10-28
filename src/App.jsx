70b79b35ed582a383c347ec44ef579a31a4e319d
zowwii <cristiancmgallego@gmail.com>
2024-10-28 17:39:32 -0500

    deply

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Home } from "./components/Pages/Home/Home"
 import { Header } from "./components/Layouts/Header/Header"
+import { AboutMe } from "./components/Pages/AboutMe/AboutMe"
 export const App = ()=> {
 
 
   return (
     <>
     <Header/>
-
+    <AboutMe/>
     </>
   )
 }
--- src/components/Pages/AboutMe/AboutMe.jsx
+++ src/components/Pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import './AboutMe.css'
 export const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <>
+    <div className='general-information'>
+      <section className='me'>
+        <p>323223</p>
+      </section>
+      <section className=''>
+        <p>
+        Soy un joven que posee conocimientos en diversas áreas, tales como la contaduría y el servicio técnico,
+        entre otros. Actualmente, estoy cursando estudios en el Servicio Nacional de Aprendizaje (SENA), donde
+        me estoy formando para obtener el tecnólogo en Análisis y Desarrollo de Software (ADSO). Este programa 
+        lo estoy realizando en modalidad nocturna, trabajo como auxiliar contable en un puesto que no es de
+        tiempo completo para haci poder tener tiempo para realizar activades de la formacion.
+        </p>
+      </section>
+    </div>
+    </>
   )
 }
