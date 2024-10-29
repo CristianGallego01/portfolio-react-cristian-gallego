5db360086669fdbb62d3a4960ff3b5dd418891d8
zowwii <cristiancmgallego@gmail.com>
2024-10-29 07:38:19 -0500

    beginning footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from "./components/Pages/Home/Home"
 import { Header } from "./components/Layouts/Header/Header"
 import { AboutMe } from "./components/Pages/AboutMe/AboutMe"
 import { Projects } from "./components/Pages/Projects/Projects"
+import { Footer } from "./components/Layouts/Footer/Footer"
 export const App = ()=> {
 
 
@@ -10,6 +11,7 @@ export const App = ()=> {
     <Header/>
     <AboutMe/>
     <Projects/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Layouts/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+
+import "./Footer.css"
+export const Footer = () =>{
+return(
+    <>
+    <section className="contact">
+    <div>
+    <h2>Contactame</h2>
+    <p>
+    Si desea ponerse en contacto, preguntas o comentarios no dude en hacerlo atravez de los siguientes medios
+    </p>
+    <p>
+    Correo : cristiancmgalleg@gmail.com
+    </p>
+    <p>
+    Telefono : +57 3184590437
+    </p>
+    </div>
+
+    <div>
+    <div className="you-name">
+    <p>
+    nombre :
+    </p>
+    <input type="text" />
+    </div>
+    </div>
+    </section>
+    
+    </>
+)
+}
